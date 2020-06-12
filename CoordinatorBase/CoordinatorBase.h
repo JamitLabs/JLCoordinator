75bceaf28d4068c705a1b9053aa14e8082c24bc6
Jens Krug <jens.krug@jamitlabs.com>
2020-06-12 12:27:52 +0200

    Initial Commit

---
+++ CoordinatorBase/CoordinatorBase.h
@@ -0,0 +1,19 @@
+//
+//  CoordinatorBase.h
+//  CoordinatorBase
+//
+//  Created by Jens on 12.06.20.
+//  Copyright © 2020 Jamit Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoordinatorBase.
+FOUNDATION_EXPORT double CoordinatorBaseVersionNumber;
+
+//! Project version string for CoordinatorBase.
+FOUNDATION_EXPORT const unsigned char CoordinatorBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoordinatorBase/PublicHeader.h>
+
+
