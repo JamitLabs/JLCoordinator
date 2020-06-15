996a974edb4cfda89b5beee6b734cc8471e45028
Jens Krug <jens.krug@jamitlabs.com>
2020-06-15 12:09:49 +0200

    Add InitialPresenter test base

---
+++ CoordinatorBaseTests/PresenterTests/InitialPresenterTests.swift
@@ -0,0 +1,16 @@
+//
+//  InitialPresenterTests.swift
+//  CoordinatorBaseTests
+//
+//  Created by Jens on 15.06.20.
+//  Copyright © 2020 Jamit Labs. All rights reserved.
+//
+
+import XCTest
+
+class InitialPresenterTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
