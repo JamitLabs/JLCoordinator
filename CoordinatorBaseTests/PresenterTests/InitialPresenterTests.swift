25b1a8d1ff7c329e8157bb678ec4377e570bed35
Jens Krug <jens.krug@jamitlabs.com>
2020-06-17 09:20:41 +0200

    Add InitialPresenter tests

--- CoordinatorBaseTests/PresenterTests/InitialPresenterTests.swift
+++ CoordinatorBaseTests/PresenterTests/InitialPresenterTests.swift
@@ -2,15 +2,36 @@
 //  InitialPresenterTests.swift
 //  CoordinatorBaseTests
 //
-//  Created by Jens on 15.06.20.
+//  Created by Jens Krug on 15.06.20.
 //  Copyright © 2020 Jamit Labs. All rights reserved.
 //
 
+import CoordinatorBase
 import XCTest
 
 class InitialPresenterTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testPresentingViewController() throws {
+        let window: UIWindow = UIWindow()
+        let presenter: InitialPresenter = InitialPresenter(window: window)
+        let viewController = UIViewController()
+        presenter.present(viewController)
+        XCTAssertTrue(presenter.window.isKeyWindow)
+        XCTAssertTrue(presenter.window.rootViewController === viewController)
+        XCTAssertTrue(viewController.isViewLoaded)
+    }
+
+    func testDismissViewController() throws {
+        // Initialize window and viewcontroller
+        let window: UIWindow = UIWindow()
+        let presenter: InitialPresenter = InitialPresenter(window: window)
+        let viewController = UIViewController()
+        presenter.present(viewController)
+        XCTAssertTrue(presenter.window.isKeyWindow)
+        XCTAssertTrue(presenter.window.rootViewController === viewController)
+        XCTAssertTrue(viewController.isViewLoaded)
+
+        // Dismiss ViewController
+        presenter.dismiss(viewController)
+        XCTAssertTrue(window.isHidden)
     }
 }
